6094300a2e54f170609cf9ecada997c7b0c6cb4e
David Crespo <crespo.dm@gmail.com>
2021-01-09 20:56:03 -0500

    clean up now that we don't need as much debug info

--- handler.js
+++ handler.js
@@ -5,27 +5,24 @@ const twitter = require("./src/twitter.js");
 const notification = require("./src/notification.js");
 const { commaSeries, setDiff } = require("./src/utils.js");
 
-module.exports.run = async (event, context) => {
+module.exports.run = async () => {
   const currFollowers = await twitter.fetchFollowers();
   const prevFollowers = await s3.getFollowers();
 
   await s3.setFollowers(currFollowers);
 
   const unfollowers = setDiff(prevFollowers, currFollowers);
-  if (unfollowers.length === 0) {
-    console.log("No unfollowers found");
-  } else {
-    console.log(
-      `Found ${unfollowers.length} unfollowers: [${unfollowers.join(", ")}]`,
-    );
-    const data = await twitter.lookup(unfollowers.slice(0, 100)); // twitter has max 100 lookup
-    if (data.length > 0) {
-      const screenNames = data.map((u) => `${u.name} (@${u.screen_name})`);
-      notification.send(`${commaSeries(screenNames)} unfollowed you.`);
-    } else {
-      notification.send(
-        `${unfollowers.length} of your followers no longer exist.`,
-      );
+  console.log(`Found ${unfollowers.length} unfollowers`);
+
+  if (unfollowers.length > 0) {
+    const users = await twitter.lookup(unfollowers.slice(0, 100)); // twitter has max 100 lookup
+
+    if (users.length === 0) {
+      notification.send(`${unfollowers.length} followers no longer exist`);
+      return;
     }
+
+    const screenNames = users.map((u) => `${u.name} (@${u.screen_name})`);
+    notification.send(`${commaSeries(screenNames)} unfollowed you`);
   }
 };
